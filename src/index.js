5868c8460f8da43d50fccc98505317bd3a25fe10
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2023-11-29 17:16:29 +0000

    Included API key and URL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "97ca3fd7c1847b8c2t8cf2o00a730a3a";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
